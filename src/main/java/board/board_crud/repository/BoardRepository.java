55cebe820b98983aaf496cd187c20e9cd3d6f3e1
cfcromn <s25058@gsm.hs.kr>
2025-07-08 18:30:10 +0900

    board

--- src/main/java/board/board_crud/repository/BoardRepository.java
+++ src/main/java/board/board_crud/repository/BoardRepository.java
@@ -3,7 +3,6 @@ package board.board_crud.repository;
 import board.board_crud.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @Repository
